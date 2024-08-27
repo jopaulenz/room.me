debd8f7719c6cc8bc3c2e1507bbe11443f33b701
mustapha ibrahim <mustalex1@gmail.com>
2024-08-27 17:18:20 +0200

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "Destroying all the contacts"
+Contact.destroy_all
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "creating contacts"
+20.times do
+  Contact.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.email,
+    address: Faker::Location.address
+  )
+end
+puts "created #{Contact.count} contacts"
