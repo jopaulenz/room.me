0722c0a255e799d7332635f961f7b0e517e68a3d
Justus Nolte <j.nolte@zeppelin-university.net>
2024-08-29 16:50:59 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Host and Flatmate routes
-  resources :hosts, only: [:new, :create] do
+  resources :hosts, only: [:new, :create, :update, :show] do
     resources :likes, only: [:create, :destroy], module: :hosts
     resources :dislikes, only: [:create, :destroy], module: :hosts
   end
-  resources :hosts, only: [:new, :create, :update, :show]
-  resources :flatmates, only: [:new, :create, :update, :show]
 
-  resources :flatmates, only: [:new, :create] do
+  resources :flatmates, only: [:new, :create, :update, :show] do
     resources :likes, only: [:create, :destroy], module: :flatmates
     resources :dislikes, only: [:create, :destroy], module: :flatmates
   end
@@ -24,14 +22,15 @@ Rails.application.routes.draw do
   get "choose_role", to: 'users#choose_role'
   patch "role", to: 'users#role'
 
+  # Steps for flatmates and hosts
   get 'flatmates/:id/step2', to: 'flatmates#edit2', as: :flatmate_edit2
   get 'flatmates/:id/step3', to: 'flatmates#edit3', as: :flatmate_edit3
 
   get 'hosts/:id/step2', to: 'hosts#edit2', as: :host_edit2
   get 'hosts/:id/step3', to: 'hosts#edit3', as: :host_edit3
 
+  # Tutorial
   get 'tutorial', to: 'pages#tutorial', as: :tutorial
-
 
   # Living preferences
   resources :living_preferences, only: [:new, :create, :edit, :update]
