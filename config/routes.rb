3a714c947c78765cc23df0c6b9f13e6e282a752c
Josephine Paulenz <josephinepaulenz@gmail.com>
2024-08-29 14:42:37 +0200

    onboarding process with profile details for flatmate

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :hosts, only: [:new, :create]
-  resources :flatmates, only: [:new, :create]
+  resources :hosts, only: [:new, :create, :update]
+  resources :flatmates, only: [:new, :create, :update]
 
   get "choose_role", to: 'users#choose_role'
 
-  get 'flatmates/:id/step2', to: 'flatmates#edit2', as: :edit2
-  patch 'flatmates/:id/step2', to: 'flatmates#update2'
+  get 'flatmates/:id/step2', to: 'flatmates#edit2', as: :flatmate_edit2
+  get 'flatmates/:id/step3', to: 'flatmates#edit3', as: :flatmate_edit3
 
-  get 'hosts/:id/step3', to: 'hostss#edit2', as: :edit3
-  patch 'hosts/:id/step3', to: 'hosts#update2'
+  get 'hosts/:id/step3', to: 'hosts#edit2', as: :host_edit2
+  get 'tutorial', to: 'pages#tutorial', as: :tutorial
 
 
   resources :living_preferences, only: [:new, :create, :edit, :update]
