b89f9364443484d81c18d69ee0b460d4bb119c3a
Emre Ebeturk <emreebeturk@gmail.com>
2024-09-02 16:55:38 +0200

    compatible seeds done

--- app/models/flatmate.rb
+++ app/models/flatmate.rb
@@ -2,5 +2,5 @@ class Flatmate < ApplicationRecord
   belongs_to :user
   has_one :living_preference, as: :preferable, dependent: :destroy
   has_many :matches, dependent: :destroy
-  has_one_attached :photo
+  has_one_attached :profile_photo
 end
