56194db6bc348fa3b9582f2d08df0cdb18fc93a2
Sascha Debatin <s.debatin@hotmail.de>
2024-09-03 15:08:28 +0200

    matches changes

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,13 @@ class Match < ApplicationRecord
   belongs_to :host
   has_many :messages, dependent: :destroy
   validates :flatmate_id, uniqueness: { scope: :host_id }
+
+  def matched_user(current_user)
+    flatmate == current_user ? host : flatmate
+  end
+
+  def last_message_content
+    messages.last&.message || "No messages yet"
+  end
 end
+
