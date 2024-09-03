579091e827ddcb5f7890df8e6b5730d14b3d263f
Justus Nolte <j.nolte@zeppelin-university.net>
2024-09-03 17:47:17 +0200

    I implemented the basic logic for suggestions based on the living preferences. Josedine you will add the logic for the host part. Please keep my first part on the host side in order to render it accordingly because on the index is everything ready for suggestions. Apart of that this code is ready to rumble.

--- app/models/flatmate.rb
+++ app/models/flatmate.rb
@@ -5,4 +5,13 @@ class Flatmate < ApplicationRecord
   has_one_attached :profile_photo
   has_many :given_likes, as: :liker, class_name: 'Like'
   has_many :liked_hosts, through: :given_likes, source: :liked, source_type: 'Host'
+
+  def suggested_hosts
+    return [] unless living_preference
+
+    Host.includes(:living_preference).map do |host|
+      score = living_preference.matching_score_with(host.living_preference)
+      { host: host, score: score }
+    end.sort_by { |suggestion| -suggestion[:score] }
+  end
 end
