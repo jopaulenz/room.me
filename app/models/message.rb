333d1ab02096b9602d45b3d8b1fede5f711a0840
Emre Ebeturk <emreebeturk@gmail.com>
2024-09-04 06:21:15 +0200

    messaging complete

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+  after_create_commit :broadcast_message
   belongs_to :match
   belongs_to :user
 
@@ -6,5 +7,14 @@ class Message < ApplicationRecord
 
   def first_name
     user.flatmate ? user.flatmate.first_name : user.host.first_name
+  end
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "match_#{match.id}_messages",
+                        partial: "messages/message",
+                        locals: { message: self, user: user },
+                        target: "messages"
   end
 end
