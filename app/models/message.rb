85eafb06772b18141873bbf538391ce2a3d71c8d
Emre Ebeturk <emreebeturk@gmail.com>
2024-09-04 05:51:46 +0200

    messaging until action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :message, presence: true, length: { maximum: 1000 }
+
+  def first_name
+    user.flatmate ? user.flatmate.first_name : user.host.first_name
+  end
 end
