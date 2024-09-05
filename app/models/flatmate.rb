d5e056250ba8dcd8a40688fcc5cda4222d1fe387
Justus Nolte <j.nolte@zeppelin-university.net>
2024-09-05 12:16:15 +0200

    fixed enum for duration on flatmate

--- app/models/flatmate.rb
+++ app/models/flatmate.rb
@@ -6,6 +6,8 @@ class Flatmate < ApplicationRecord
   has_many :given_likes, as: :liker, class_name: 'Like'
   has_many :liked_hosts, through: :given_likes, source: :liked, source_type: 'Host'
 
+  enum duration: { longterm: 0, shortterm: 1 }
+
   def age
     now = Time.now.utc.to_date
     now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
